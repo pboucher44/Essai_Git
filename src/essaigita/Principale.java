1643bfeca4b501115fa638a822518c1a6bb9598a
pboucher <pboucher@btssio-VirtualBox>
2017-09-07 17:31:45 +0200

    Ajout d'un text a destination de B

--- src/essaigita/Principale.java
+++ src/essaigita/Principale.java
@@ -16,6 +16,7 @@ public class Principale {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Bonjour de A");
     }
     
 }
